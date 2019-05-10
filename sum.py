35917363ec55f8e20b7dc0086278eeff31377f00
Vivek Singh <viveksingh@Viveks-MacBook-Pro.local>
2019-05-10 01:35:53 -0400

    changes made to test

--- sum.py
+++ sum.py
@@ -1,7 +1,11 @@
 x = 20
 y = 60
 z = 60
+a = 234
 print "This is the sum of x and y:"
 sum = x + y + z
 print sum
 print "hello, this is last change on github sever i.e branching"
+
+print "new change of temp branch"
+
